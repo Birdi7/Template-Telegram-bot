4732022c17541bafd49ba102d7982414c79ffc86
UnicornsExist <kamil1999riz@gmail.com>
2019-07-02 18:53:11 +0300

    added uMongo support to models

--- core/db_models.py
+++ core/db_models.py
@@ -1,7 +1,12 @@
-from mongoengine import Document
-from mongoengine.fields import IntField, StringField
+from umongo import Document, Instance
+from umongo.fields import IntField, StringField
+from motor import motor_asyncio
+
+client = motor_asyncio.AsyncIOMotorClient()
+instance = Instance(client.test_db)
 
 
+@instance.register
 class User(Document):
     chat_id = IntField(unique=True)
     first_name = StringField()

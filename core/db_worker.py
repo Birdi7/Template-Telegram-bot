c139c5f7a273b7d9b8caa818a6b56aa5707aa28f
Egor <egorr.osokin@gmail.com>
2019-07-02 23:04:14 +0300

    Fix update_user

--- core/db_worker.py
+++ core/db_worker.py
@@ -1,24 +1,18 @@
 import asyncio
 
-from core.db_models import instance, User
 from motor import motor_asyncio
+
+from core.db_models import instance, User
 
 
 async def update_user(chat_id, **kwargs):
     """
         Обновление информации о пользователе
     """
-    user = await User.find_one({"chat_id": chat_id})
-
-    if not user:
-        user = User(chat_id=chat_id, **kwargs)
-        await user.commit()
-    else:
-        user.update(**kwargs)
-        await user.commit()
+    await User.collection.update_one({"chat_id": chat_id}, {"$set": kwargs}, upsert=True)
 
 
-client = motor_asyncio.AsyncIOMotorClient(host="mongodb+srv://birdi7:A0DdtfvGpLn3yha4@cluster0-aqa0h.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = motor_asyncio.AsyncIOMotorClient(host="mongodb+srv://birdi7:A0DdtfvGpLn3yha4@cluster0-aqa0h.gcp.mongodb.net/test_db?retryWrites=true&w=majority")
 instance.init(client.test_db)
 
 asyncio.get_event_loop().create_task(User.ensure_indexes())

08e10e78340f0453dc76d5baae46688d9219c0ac
UnicornsExist <kamil1999riz@gmail.com>
2019-07-03 17:23:28 +0300

    Trully renamed core to utils

---
+++ utils/states.py
@@ -0,0 +1,9 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+class FeedbackDialog(StatesGroup):
+    enter_feedback = State()
+
+
+class SendToEveryoneDialog(StatesGroup):
+    enter_message = State()
